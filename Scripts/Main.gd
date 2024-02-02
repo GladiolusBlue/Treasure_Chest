7b9c7418c1eecc55b4d0b1b7460a21afc045b36c
Rhislav <91127795+Rhislav@users.noreply.github.com>
2024-02-03 01:29:20 +0300

    победил перемещение кубов

--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -1,8 +1,8 @@
 extends Node
 var gotov = 0
 var cube = 0
-var g = 0
-var c = 0
+var a = 0
+var b = 0
 
 
 # Called when the node enters the scene tree for the first time.
@@ -24,21 +24,30 @@ func _process(delta):
 
 func _on_button_1_pressed():
 	gotov = 0
-	$Dice_e1/Dice_main.fall_dice()
-	#print("test1","-",x,"-",y,"-",gotov)
+	a = 19
+	b = -550
+	$Dice_e1/Dice_main.fall_dice(a,b)
+	#print("test1","-",a,"-",b,"-",gotov)
 	
 
 # запуск следующего куба по готовности
 func next1(x,y):
 	gotov += 1
 	if gotov == 1:
-		$Dice_e2/Dice_main.fall_dice()
+		a = 700
+		b = -550
+		$Dice_e2/Dice_main.fall_dice(a,b)
 		print("E1","-",x,"-",y)
 	elif gotov == 2:
-		$Dice_p1/Dice_main.fall_dice()
+		a = 19
+		b = 550
+		$Dice_p1/Dice_main.fall_dice(a,b)
 		print("E2","-",x,"-",y)
 	elif gotov == 3:
-		$Dice_p2/Dice_main.fall_dice()
+		a = 700
+		b = 550
+		$Dice_p2/Dice_main.fall_dice(a,b)
+		
 		print("P1","-",x,"-",y)
 	else :
 		print("P2","-",x,"-",y)
